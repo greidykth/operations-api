43341d9436022704e7dbd2b6528396d1dfe2f4e2
Greidy Peña <greidykp@gmail.com>
2021-08-12 21:04:08 -0500

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\BalanceController;
+use App\Http\Controllers\ResetController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/reset', [ResetController::class, 'reset']);
+
+Route::get('/balance', [BalanceController::class, 'show']);
+
+Route::post('/event', [BalanceController::class, 'store']);
